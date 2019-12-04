36b802d0c1021262bcd1dc4a23acbefe75c5093f
niceLemonGuy <46706839+niceLemonGuy@users.noreply.github.com>
2019-12-04 22:37:19 +0300

    Update mywindow.h

--- src/mywindow.h
+++ src/mywindow.h
@@ -15,12 +15,23 @@
 #include <QList>
 #include <QString>
 
+#define sz 10
+
 class MyWindow : public QDialog
 {
     Q_OBJECT
 
 public:
-    MyWindow(QWidget *parent=0);
+    MyWindow(QWidget *parent = nullptr);
+
+    int op = 0, mass[sz], val = 0, tr_mn = 10, mn = 10;
+    int n_1 = 0, n_2 = 0; double x10Val;
+    int count = 0, count_init = 0;
+    bool addBool = false, subBool = false, multBool = false;
+    bool divBool = false, transBool = false, n_2Bool = false;
+
+    QString value = "";
+
 private slots:
     void actions();
     void initial();
@@ -31,8 +42,10 @@ private slots:
     void clear();
     void text();
     void setValue();
+
 signals:
     void valueChanged(int);
+
 private:
     QFont *font;
     QSpinBox *sb1;
